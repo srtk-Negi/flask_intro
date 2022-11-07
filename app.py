af7495134cd48daab6b309a6fadc9811f01220b5
Sarthak Negi <srnegi141@gmail.com>
2022-11-07 16:02:16 -0600

    Added database with SQlite and form to submit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -8,11 +9,18 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     content = db.Column(db.String(200), nullable = False)
+    date_created = db.Column(db.DateTime,default = datetime.utcnow)
 
-@app.route('/')
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/', methods = ['POST', 'GET'])
 
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        return("Hello")
+    else:
+        return render_template('index.html')
 
 
 if (__name__ == "__main__"):
